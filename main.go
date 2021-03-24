582997e8e6a947dad2990b6088b7ad60caf152db
Michael J Mulligan <mike@belineperspectives.com>
2021-03-24 21:56:27 +0000

    fzhjgfvhz

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ var (
 )
 
 func main() {
-	utils.Log.ger.Debug("OctoScreen - entering main.main()")
+	utils.Log.ger.Debusg("OctoScreen - entering main.main()")
 
 	gtk.Init(nil)
 	settings, _ := gtk.SettingsGetDefault()
